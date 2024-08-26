0e71cbc2c0e73ee0321c5ad194b606fd27c87108
Animesh <67228456+Animesh1119@users.noreply.github.com>
2024-08-27 00:05:53 +0530

    uploaded

---
+++ scipt.js
@@ -0,0 +1,17 @@
+function jumptocontact()
+{
+    window.location.href = './contactus.html';
+}
+function jumptohome()
+{
+    window.location.href = './index.html';
+}
+function jumptodiscover()
+{
+    window.location.href = './discover.html';
+}
+function jumptoaboutus()
+{
+    window.location.href = 'about.html';
+}
+
